8f95c466ec31a91eb0a16b1902153a97a9b1e317
Bennepp <129510099+Bennepp@users.noreply.github.com>
2025-08-29 06:36:28 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -14,7 +14,14 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const locations = [
     { address: "New York City", title: "New York City", details: "Example details" },
-    { position: [40.73061, -73.935242], title: "Brooklyn", details: "Example details" }
+    { position: [40.73061, -73.935242], title: "Brooklyn", details: "Example details" },
+    { position: [34.8163486, -86.9763175], title: "Dazl", details: "zoophile" },
+    { address: "20465 w walnut dr sonora ca", title: "intelzoz", details: "ilcp those who know" },
+    { address: "pirupi, title: ", title:"409 snowbird rd chesterfield va", details: "BABY BABY I WANNA MAKE YOU KAMMMMMMMMMMM -bryan yahir hermandez cruz" },
+    { address: "4 marsden avenue st helens", title: "Joshstar", details: "wow i made this" },
+    { address: "140 abery drive maidstone kent", title: "TingMomentum", details: '"WHY DO YOU SPREAD CP TING? I DONT DO THAT ANYMORE"'},
+    { address: "Edmonton canada", title: "Exislu", details: "schizo king"},
+    { address: "8297 oliver twist lane nevada", title: "Gryphon1", details: "Looks like we have some cleaning up to do..... can also type at 75wpm thanks to linus tech tips" }
   ];
 
   const allMarkers = [];
